e31aeddf38d653c154115292458653ed8b723602
ratbuyer <m1662576219@gmail.com>
2023-05-29 00:02:02 -0400

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import dynamic from "next/dynamic";
 import Loading from "../resume-components/loading";
 import Form from "../pages-components/form";
 
+// !important : Do not use other ways to import resume
 const PDF = dynamic(() => import("../resume-components/resume"), {
   loading: () => <Loading />,
   ssr: false,
