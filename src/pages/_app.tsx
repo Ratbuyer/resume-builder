bda0653491d9098465baa4a31fa53f4e93a4a4fc
ratbuyer <m1662576219@gmail.com>
2023-06-14 15:51:41 -0400

    vercel analysis

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 
   return <>
     <title>Resume Dev Tool</title>
     <Component {...pageProps} />
+    <Analytics />
   </>;
 };
 
