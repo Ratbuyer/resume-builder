cc31f7896099f76bf1b899763b5eea5197d86345
ratbuyer <m1662576219@gmail.com>
2023-06-15 22:36:06 -0400

    phone size

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,30 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { Analytics } from '@vercel/analytics/react';
+import Loading from "~/components/loading";
+import { useState, useEffect } from "react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [deviceType, setDeviceType] = useState(0);
+
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setDeviceType(0);
+    } else {
+      setDeviceType(1);
+    }
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (!deviceType) {
+    return <p className="text-center mt-20">please use a larger device for a better experience</p>;
+  }
 
   return <>
     <title>Resume Dev Tool</title>
