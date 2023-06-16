ad5f95789ae5dfb2bd77283bbec04ae298652d4a
ratbuyer <m1662576219@gmail.com>
2023-06-16 02:02:00 -0400

    tweak

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -2,7 +2,9 @@ export const COLORTABLE: [string, string][] = [
   ["White", "#ffffff"],
   ["Ivory", "#FFFFF0"],
   ["Lemon", "#FFFACD"],
-  ["Ocean Blue", "#ADD8E6"],
+  ["Ocean", "#ADD8E6"],
+  ["Light Blue", "#B3C7F9"],
+  ["Light Rose", "#FED7D7"],
 ];
 
 export const FONTS: string[] = [
