9edc9b3d316fb4119968b74811f7c3dbc8923953
ratbuyer <m1662576219@gmail.com>
2023-06-21 03:39:44 -0400

    extract svg from buttons

--- src/components/pages/buttons.tsx
+++ src/components/pages/buttons.tsx
@@ -1,17 +1,7 @@
 import Refresh from "@public/assets/refresh.svg";
 import Setting from "@public/assets/setting.svg";
-
-const MinusIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5 text-gray-600">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18 12H6"></path>
-  </svg>
-);
-
-const PlusIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5 text-gray-600">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-  </svg>
-);
+import MinusIcon from "@public/assets/minus.svg";
+import PlusIcon from "@public/assets/plus.svg";
 
 
 export const RefreshButton = ({ handleSubmit }: { handleSubmit: () => void }) => (
