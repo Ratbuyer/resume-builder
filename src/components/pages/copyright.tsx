34809d462a940c32a8f8b93626775234289a9c8d
ratbuyer <m1662576219@gmail.com>
2023-06-21 17:46:58 -0400

    update

--- src/components/pages/copyright.tsx
+++ src/components/pages/copyright.tsx
@@ -1,18 +1,34 @@
-
+import { useState } from 'react';
+import { set } from 'zod';
 
 const Copyright = () => {
-  
+
   const authorName = 'resumedev.site';
   const email = "m1662576219@gmail.com";
 
-  return (
+  const [showFeedback, setShowFeedback] = useState(false);
+
+
+  return <>
+
     <div
-      className="mt-10 text-center text-gray-500 text-sm py-4 flex flex-col"
+      className="mt-10 text-gray-500 text-sm py-10 px-10 
+      flex flex-row justify-between items-center"
     >
+
       <p>&copy; {authorName}</p>
-      <p>Contact us by {email}</p>
+
+      <button
+        className=""
+        onClick={() => setShowFeedback(true)}
+      >give us feedback
+      </button>
+
     </div>
-  );
+
+    {showFeedback && <div className="fixed top-0 left-0 w-screen h-screen bg-black opacity-50 z-10"></div>}
+
+  </>
 };
 
 export default Copyright;
---
+++ src/server/api/routers/feedback.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+
+export const feedbackRouter = createTRPCRouter({
+  createFeedback: publicProcedure
+    .input(z.object({ text: z.string(), email: z.optional(z.string().email()) }))
+    .mutation(({ input, ctx }) => {
+
+      const response = ctx.prisma.feedback.create({
+        data: { text: input.text, email: input.email }
+      });
+
+      return response;
+    })
+
+});
