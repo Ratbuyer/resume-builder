c2212ad8f0361b27ef0b3e0124920dce196ef703
ratbuyer <m1662576219@gmail.com>
2023-07-02 16:12:32 -0400

    settings tweak

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,36 +14,56 @@ const PDF = dynamic(() => import("../components/resume"), {
   ssr: false,
 });
 
-
 const Index = () => {
-
   const [loading, setLoading] = useState<boolean>(true);
-  const [settings, setSettings] = useState<types.settingsType>(defaults.settings);
+  const [settings, setSettings] = useState<types.settingsType>(
+    defaults.settings
+  );
 
   const [header, setHeader] = useState<types.headerType>(defaults.header);
-  const [educationList, setEducationList] = useState<types.educationListType>(defaults.educationList);
-  const [skillsList, setSkillsList] = useState<types.skillsListType>(defaults.skillsList);
-  const [awardsList, setAwardsList] = useState<types.awardsListType>(defaults.awardsList);
-  const [experienceList, setExperienceList] = useState<types.experienceListType>(defaults.experienceList);
-  const [projectList, setProjectList] = useState<types.projectsListType>(defaults.projectList);
+  const [educationList, setEducationList] = useState<types.educationListType>(
+    defaults.educationList
+  );
+  const [skillsList, setSkillsList] = useState<types.skillsListType>(
+    defaults.skillsList
+  );
+  const [awardsList, setAwardsList] = useState<types.awardsListType>(
+    defaults.awardsList
+  );
+  const [experienceList, setExperienceList] =
+    useState<types.experienceListType>(defaults.experienceList);
+  const [projectList, setProjectList] = useState<types.projectsListType>(
+    defaults.projectList
+  );
 
   const formRef = useRef<HTMLFormElement>(null);
 
   const handleSubmit = () => {
-
     const form = formRef.current;
 
     if (!form?.checkValidity()) {
-      form?.reportValidity()
+      form?.reportValidity();
       return;
     }
 
-    const headerCopy = JSON.parse(form?.dataset.header ? form.dataset.header : "{}") as types.headerType
-    const educationListCopy = JSON.parse(form?.dataset.educationList ? form.dataset.educationList : "[]") as types.educationListType
-    const skillsListCopy = JSON.parse(form?.dataset.skillsList ? form.dataset.skillsList : "[]") as types.skillsListType
-    const awardsListCopy = JSON.parse(form?.dataset.awardsList ? form.dataset.awardsList : "[]") as types.awardsListType
-    const experienceListCopy = JSON.parse(form?.dataset.experienceList ? form.dataset.experienceList : "[]") as types.experienceListType
-    const projectListCopy = JSON.parse(form?.dataset.projectsList ? form.dataset.projectsList : "[]") as types.projectsListType
+    const headerCopy = JSON.parse(
+      form?.dataset.header ? form.dataset.header : "{}"
+    ) as types.headerType;
+    const educationListCopy = JSON.parse(
+      form?.dataset.educationList ? form.dataset.educationList : "[]"
+    ) as types.educationListType;
+    const skillsListCopy = JSON.parse(
+      form?.dataset.skillsList ? form.dataset.skillsList : "[]"
+    ) as types.skillsListType;
+    const awardsListCopy = JSON.parse(
+      form?.dataset.awardsList ? form.dataset.awardsList : "[]"
+    ) as types.awardsListType;
+    const experienceListCopy = JSON.parse(
+      form?.dataset.experienceList ? form.dataset.experienceList : "[]"
+    ) as types.experienceListType;
+    const projectListCopy = JSON.parse(
+      form?.dataset.projectsList ? form.dataset.projectsList : "[]"
+    ) as types.projectsListType;
 
     setHeader(headerCopy);
     setEducationList(educationListCopy);
@@ -61,7 +81,6 @@ const Index = () => {
   };
 
   useEffect(() => {
-
     const settings = localStorage.getItem("settings");
     const header = localStorage.getItem("header");
     const educationList = localStorage.getItem("educationList");
@@ -71,54 +90,71 @@ const Index = () => {
     const projectList = localStorage.getItem("projectList");
 
     header && setHeader(JSON.parse(header) as types.headerType);
-    educationList && setEducationList(JSON.parse(educationList) as types.educationListType);
+    educationList &&
+      setEducationList(JSON.parse(educationList) as types.educationListType);
     skillsList && setSkillsList(JSON.parse(skillsList) as types.skillsListType);
     awardsList && setAwardsList(JSON.parse(awardsList) as types.awardsListType);
-    experienceList && setExperienceList(JSON.parse(experienceList) as types.experienceListType);
-    projectList && setProjectList(JSON.parse(projectList) as types.projectsListType);
-    settings && Object.values(settings).every(Boolean) && setSettings(JSON.parse(settings) as types.settingsType);
+    experienceList &&
+      setExperienceList(JSON.parse(experienceList) as types.experienceListType);
+    projectList &&
+      setProjectList(JSON.parse(projectList) as types.projectsListType);
+    settings &&
+      Object.values(settings).every(Boolean) &&
+      setSettings(JSON.parse(settings) as types.settingsType);
 
     setLoading(false);
   }, []);
 
   const Left = () => {
     if (loading) return <Loading />;
-    return <>
-      <Form
-        func={handleSubmit}
-        headerprop={header}
-        educationListprop={educationList}
-        skillsListprop={skillsList}
-        awardsListprop={awardsList}
-        experienceListprop={experienceList}
-        projectListprop={projectList}
-        refObject={formRef}
-      />
-      <Copyright />
-    </>
+    return (
+      <>
+        <Form
+          func={handleSubmit}
+          headerprop={header}
+          educationListprop={educationList}
+          skillsListprop={skillsList}
+          awardsListprop={awardsList}
+          experienceListprop={experienceList}
+          projectListprop={projectList}
+          refObject={formRef}
+        />
+        <Copyright />
+      </>
+    );
   };
 
   const Middle = () => {
-
     const [showSetting, setShowSetting] = useState(false);
 
-    return <>
+    return (
+      <>
+        <div
+          className="flex flex-col place-content-center items-center 
+          justify-center gap-y-10"
+        >
+          <RefreshButton handleSubmit={handleSubmit} />
+          <SettingButton
+            func={() => {
+              setShowSetting(true);
+            }}
+          />
+        </div>
 
-      <div className="flex flex-col justify-center items-center 
-          place-content-center gap-y-10">
-        <RefreshButton handleSubmit={handleSubmit} />
-        <SettingButton func={() => { setShowSetting(true) }} />
-      </div>
-
-      {showSetting && <div className="fixed top-0 left-0 w-screen h-screen bg-black opacity-50 z-10"></div>}
-
-      <Settings
-        isOpen={showSetting}
-        onClose={() => { setShowSetting(false) }}
-        settings={settings}
-        setSettings={setSettings}
-      />
-    </>
+        {showSetting && (
+          <div className="fixed left-0 top-0 z-10 h-screen w-screen bg-black opacity-50"></div>
+        )}
+        {showSetting && (
+          <Settings
+            onClose={() => {
+              setShowSetting(false);
+            }}
+            settings={settings}
+            setSettings={setSettings}
+          />
+        )}
+      </>
+    );
   };
 
   const Right = () => (
@@ -133,21 +169,23 @@ const Index = () => {
     />
   );
 
-  return <>
-    <div className="flex justify-between h-screen">
-      <div className="w-1/2 pt-0 p-2 text-center overflow-auto">
-        <Left />
-      </div>
+  return (
+    <>
+      <div className="flex h-screen justify-between">
+        <div className="w-1/2 overflow-auto p-2 pt-0 text-center">
+          <Left />
+        </div>
 
-      <div className="flex flex-col justify-center items-center place-content-center p-3">
-        <Middle />
-      </div>
+        <div className="flex flex-col place-content-center items-center justify-center p-3">
+          <Middle />
+        </div>
 
-      <div className="w-1/2 pt-0">
-        <Right />
+        <div className="w-1/2 pt-0">
+          <Right />
+        </div>
       </div>
-    </div>
-  </>
+    </>
+  );
 };
 
 export default Index;
